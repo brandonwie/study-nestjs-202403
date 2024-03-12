c4c02e97b63f346e926ae71012e0da9d14eadb2b
Brandon Wie <brandonwie.cs@gmail.com>
2024-03-12 23:07:00 +0900

    feat: define AppController and AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { Controller, Module } from '@nestjs/common';
+import { Controller, Get, Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'; // very few things are exported from core
 
 // decorator - nest makes use of decorators heavily
 @Controller()
 class AppController {
-  getRootRoute() {}
+  @Get()
+  getRootRoute() {
+    return 'hi there!';
+  }
 }
+
+// expected to pass config option/object to module
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+// just conventional fn name
+async function bootstrap() {
+  // create instance
+  const app = await NestFactory.create(AppModule);
+
+  // Express와 비슷하다
+  await app.listen(3000);
+}
+
+bootstrap();
